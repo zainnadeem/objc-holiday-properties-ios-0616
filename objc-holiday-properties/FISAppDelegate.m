d03a57e1f3e5a76efe3472628e1beb3e955bad48
markedwardmurray <markedwardmurray@gmail.com>
2015-08-05 13:28:10 -0400

    mark's solution

--- objc-holiday-properties/FISAppDelegate.m
+++ objc-holiday-properties/FISAppDelegate.m
@@ -21,34 +21,40 @@
 }
 
 - (NSArray *)holidaysInSeason:(NSString *)season {
-    return nil;
+    
+    return [self.database[season] allKeys];
 }
 
 - (NSArray *)suppliesInHoliday:(NSString *)holiday
                       inSeason:(NSString *)season {
-    return nil;
+    
+    return [NSArray arrayWithArray:self.database[season][holiday] ];
 }
 
 - (BOOL)holiday:(NSString* )holiday
      isInSeason:(NSString *)season {
-    return nil;
+    
+    return [[self.database[season] allKeys] containsObject:holiday];
 }
 
 - (BOOL)supply:(NSString *)supply
    isInHoliday:(NSString *)holiday
       inSeason:(NSString *)season {
-    return nil;
+    
+    return [self.database[season][holiday] containsObject:supply];
 }
 
 - (void)addHoliday:(NSString *)holiday
           toSeason:(NSString *)season {
     
+    self.database[season][holiday] = [[NSMutableArray alloc] init];
 }
 
 - (void)addSupply:(NSString *)supply
         toHoliday:(NSString *)holiday
          inSeason:(NSString *)season {
     
+    [self.database[season][holiday] addObject:supply];
 }
 
 
